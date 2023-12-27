7b30fcaf188db98fdf13513841263ae9d22bb5b1
AS0407 <90244401+AS0407@users.noreply.github.com>
2023-12-27 15:55:50 +0530

    Update index.js

--- .github/actions/hello /index.js
+++ .github/actions/hello /index.js
@@ -1,4 +1,6 @@
-const core = require("@actions/core");
+//This index.js file will return an error.
+const core = require("@actions/core");  //This @actions/core is not present in our repository. These are the node modules so for that we have to complie the action's js code into one file.
+//Now, we'll make use of vercel/ncc commands to compile the index.js so that it can include all the required packages bundled in one file.
 const github = require("@actions/github");
 
 try {
